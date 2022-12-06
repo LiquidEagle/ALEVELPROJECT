2e0f7c48dedc29b2590e2aeb6d8fac558efd0a5c
LiquidEagle <46903561+LiquidEagle@users.noreply.github.com>
2022-12-06 19:28:22 +0000

    changed the way jumping works

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from  config import *  #this contains all the global level variables across the 
 from classes import * #this imports the two classes from the classes.py
 import databaseActions #all database actions are here
 
-#700, 398
+#700, 398 
 #main Pygame drawing loop function
 def pygame_start():
 	pygame.init()
-	global world, vel_y, dy, dx
+	global world, vel_y, dy, dx, isJump
 	#set variable locations
 	mountain = Mountain(0, 0)
 	user = Player(px, ply)  
@@ -23,11 +23,13 @@ def pygame_start():
 	done = True
 	while done:
 		
-
+		
 		keys_pressed = pygame.key.get_pressed()
-
-		if keys_pressed[pygame.K_SPACE]:
-			vel_y = -15
+		if keys_pressed[pygame.K_SPACE] and isJump ==False:
+			vel_y = -5
+			isJump = True
+		if keys_pressed[pygame.K_SPACE] == False:
+			isJump = False
 		if keys_pressed[pygame.K_LEFT] and user.rect.x > dx:
 			user.rect.x -= dx
 			user.move_left = True
@@ -37,41 +39,19 @@ def pygame_start():
 			user.rect.x += dx
 			user.move_left = False
 			user.move_right = True
-
 		else:
 			user.move_left = False
 			user.move_right = False
 			user.stepIndex = 0
 
-		for tile in world.tile_list:
- 			#check for collision in x direction
-			if tile[1].colliderect(user.rect.x + dx, user.rect.y, user.width, user.height):
-				dx = 0
-		for tile in world.tile_list:
-			#check for y collison (tile stored in 1 and image in 0)
-			if tile[1].colliderect(user.rect.x, user.rect.y + dy, user.width, user.height):
-				#check if below ground (jumping)
-				if vel_y < 0:
-					vel_y = tile[1].bottom - user.rectR.top
-					vel_y = 0
-				elif vel_y >= 0:
-					vel_y = tile[1].top - user.rectR.bottom
-					vel_y = 0
-
-			#check for y collison (tile stored in 1 and image in 0)
-			elif tile[1].colliderect(user.rect.x, user.rect.y + dy, user.width, user.height):
-				#check if below ground (jumping)
-				if vel_y < 0:
-					vel_y = tile[1].bottom - user.rectL.top
-					vel_y = 0
-				elif vel_y >= 0:
-					vel_y = tile[1].top - user.rectL.bottom
-					vel_y = 0
+		
 		vel_y += 1
 		if vel_y > 10:
 			vel_y = 10
+		
 		dy += vel_y
-
+		user.rect.y += dy
+		#temp
 		if user.rect.bottom > screen_height:
 			user.rect.bottom = screen_height
 			dy = 0
@@ -82,7 +62,21 @@ def pygame_start():
 				done = False
 				pygame.quit()	
 		
-		
+		for tile in world.tile_list:
+ 			#check for collision in x direction
+			if tile[1].colliderect(user.rect.x + dx, user.rect.y, user.width, user.height):
+				dx = 0
+
+			#check for y collison (tile stored in 1 and image in 0)
+			if tile[1].colliderect(user.rect.x, user.rect.y + dy, user.width, user.height):
+				#check if below ground (jumping)
+				if vel_y < 0:
+					vel_y = tile[1].bottom - user.rect.top
+					vel_y = 0
+				elif vel_y >= 0:
+					vel_y = tile[1].top - user.rect.bottom
+					vel_y = 0
+					
 		# draw background 
 		mountain.draw() 
 		world.draw()
